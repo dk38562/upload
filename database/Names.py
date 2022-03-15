bba088f95116e8a95d7470a921d9bf3f22681f54
dk38562 <98122498+dk38562@users.noreply.github.com>
2022-03-15 11:38:16 -0700

    a

--- database/Names.py
+++ database/Names.py
@@ -1,3 +1,6 @@
-x = u"1"
-y = x.isnumeric()
-print (y)
+import ast
+with open('numbers.txt') as f:
+    data = f.read()
+members = ast.literal_eval(data)
+x = raw_input("name ")
+members[3] = x
--- database/index.py
+++ database/index.py
@@ -1,5 +1,8 @@
+import ast
+with open('numbers.txt') as f:
+    data = f.read()
+members = ast.literal_eval(data)
 while True:
-    # valid query check:
     while True:
         goal = raw_input("Do you want to check a member or make a new entry? NewMem/Check ")
         if goal.lower() != "newmem" and goal.lower() != "check":
@@ -9,19 +12,11 @@ while True:
     if goal.lower() == "newmem":
         print(goal.lower())
     if goal.lower() == "check":
-        alan = ("Name: Alan Pan", "Member Since: 03/23/07", "Email: alanpan23@gmail.com")
-        maria = ("Name: Maria Pan", "Member Since: 03/24/07", "Email: mariapan24@gmail.com")
-        martin = ("Name: Martin Novak", "Member Since: 11/12/19", "Email: novakomole@gmail.com")
-        members = {
-            "1234567890": alan,
-            "0987654321": maria,
-            "1293045672": martin,
-        }
         x = raw_input("enter phone number here ")
-        # print out the list
         try:
             for i in range(len(members[x])):
                 print((members[x])[i])
+
         except:
             if x.isdigit() == False:
                 print ("input has letters; try again")
