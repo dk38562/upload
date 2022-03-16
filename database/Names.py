5109055533821175ab04a84d039264f612904745
That-Dude111 <72678136+That-Dude111@users.noreply.github.com>
2022-03-16 00:57:51 -0700

    a

--- database/Names.py
+++
@@ -1,6 +0,0 @@
-import ast
-with open('numbers.txt') as f:
-    data = f.read()
-members = ast.literal_eval(data)
-x = raw_input("name ")
-members[3] = x
--- database/index.py
+++ database/index.py
@@ -1,31 +1,54 @@
+import datetime
 import ast
+date = datetime.datetime.now()
 with open('numbers.txt') as f:
     data = f.read()
 members = ast.literal_eval(data)
+f.close()
 while True:
     while True:
-        goal = raw_input("Do you want to check a member or make a new entry? NewMem/Check ")
+        goal = input("Do you want to check a member or make a new entry? NewMem/Check ")
         if goal.lower() != "newmem" and goal.lower() != "check":
             print("invalid query")
         else:
             break
     if goal.lower() == "newmem":
-        print(goal.lower())
+        while True:
+            num = input("phone number: ")
+            if not num.isdigit():
+                print("input has letters; try again")
+            elif len(num) < 10:
+                print("sorry that number is too short; try again")
+            elif len(num) > 10:
+                print("sorry that number is too long; try again")
+            else:
+                break
+        name1 = input("first name: ").title()
+        name2 = input("last name: ").title()
+        mem = date.strftime("%x")
+        email = input("email: ")
+        members[num] = ('Name: {} {}'.format(name1, name2), 'Member Since: {}'.format(mem), 'Email: {}'.format(email))
+        f = open("numbers.txt", "w")
+        f.write(str(members))
+        f.close()
+        print()
+        for i in range(len(members[num])):
+            print((members[num])[i])
+        print()
     if goal.lower() == "check":
-        x = raw_input("enter phone number here ")
+        x = input("enter phone number here ")
         try:
             for i in range(len(members[x])):
                 print((members[x])[i])
-
         except:
-            if x.isdigit() == False:
-                print ("input has letters; try again")
+            if not x.isdigit():
+                print("input has letters; try again")
             elif len(x) < 10:
-                print ("sorry that number is too short; try again")
+                print("sorry that number is too short; try again")
             elif len(x) > 10:
-                print ("sorry that number is too long; try again")
+                print("sorry that number is too long; try again")
             elif len(x) == 10:
                 print("sorry that number is not in our system; try again")
             else:
-                print ("sorry invalid request; try again")
-        print ("")
+                print("sorry invalid request; try again")
+        print("")
