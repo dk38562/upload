052706183b48ed9cc41203685718b719d40b9d1c
dk38562 <98122498+dk38562@users.noreply.github.com>
2022-03-17 11:39:12 -0700

    a

--- database/main.py
+++ database/main.py
@@ -7,14 +7,14 @@ members = ast.literal_eval(data)
 f.close()
 while True:
     while True:
-        goal = input("Do you want to check a member or make a new entry? NewMem/Check ")
+        goal = raw_input("Do you want to check a member or make a new entry? NewMem/Check ")
         if goal.lower() != "newmem" and goal.lower() != "check":
             print("invalid query")
         else:
             break
     if goal.lower() == "newmem":
         while True:
-            num = input("phone number: ")
+            num = raw_input("phone number: ")
             if not num.isdigit():
                 print("input has letters; try again")
             elif len(num) < 10:
@@ -23,21 +23,21 @@ while True:
                 print("sorry that number is too long; try again")
             else:
                 break
-        name1 = input("first name: ").title()
-        name2 = input("last name: ").title()
+        name1 = raw_input("first name: ").title()
+        name2 = raw_input("last name: ").title()
         mem = date.strftime("%x")
-        email = input("email: ")
+        email = raw_input("email: ")
         members[num] = ('Name: {} {}'.format(name1, name2), 'Member Since: {}'.format(mem), 'Email: {}'.format(email))
         f = open("numbers.txt", "w")
         f.write(str(members))
         f.close()
-        print()
+        print("")
         for i in range(len(members[num])):
             print((members[num])[i])
-        print()
+        print("")
     if goal.lower() == "check":
-        x = input("enter phone number here ")
-        print()
+        x = raw_input("enter phone number here ")
+        print("")
         try:
             for i in range(len(members[x])):
                 print((members[x])[i])
