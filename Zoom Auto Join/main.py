670ada8add6004deab25bce4767553d589aea1f7
dk38562 <98122498+dk38562@users.noreply.github.com>
2022-04-19 13:40:22 -0700

    a

--- Zoom Auto Join/main.py
+++ Zoom Auto Join/main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
-import selenium
-# Using Chrome to access web
+
+
 driver = webdriver.Chrome()
-# Open the website
-driver.get('https://canvas.case.edu')
+
+driver.get("http://selenium.dev")
+
+driver.quit()
