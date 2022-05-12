59186f6d1c7426a1b82dec836e1ce17d4cdf6aa2
That-Dude111 <72678136+That-Dude111@users.noreply.github.com>
2022-05-12 08:08:37 -0700

    a

---
+++ mountable/back animation/other thing copy/script.js
@@ -0,0 +1,41 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  const intro = document.querySelector(".intro");
+
+  const controller = new ScrollMagic.Controller();
+  const elem = document.getElementById("lottie");
+  let anim;
+
+  // set desired animation time
+  // 809 frames - 3x
+  const animationTime = 2427;
+
+  let scene = new ScrollMagic.Scene({
+    duration: animationTime,
+    triggerElement: intro,
+    triggerHook: 0,
+  })
+    .setPin(intro)
+    .addTo(controller);
+
+  // Video animation
+  let delay = 0;
+  let heightPerFrame = 0;
+
+  scene.on("update", (e) => {
+    heightPerFrame = anim.totalFrames / animationTime;
+    delay = Math.round(e.scrollPos * heightPerFrame);
+    anim.goToAndStop(delay, true);
+  });
+
+  const animateData = {
+    container: elem, //
+    renderer: "svg",
+    loop: false,
+    autoplay: false,
+    rendererSettings: { progressiveLoad: false },
+    path:
+      "https://raw.githubusercontent.com/dk38562/upload/main/back%20animation/data.json",
+  };
+
+  anim = lottie.loadAnimation(animateData);
+});
--- mountable/index.js
+++
@@ -1,8 +0,0 @@
-$window = $(window);
-$(window).scroll(function(){
-
-if($window.scrollTop() > 300)
-    $("#header").addClass('active');
-else    
-    $("#header").removeClass('active');
-});
